a673c0470edeae0fc72b52f83033c4ffc673a1a4
Erik Sundell <erik.sundell87@gmail.com>
2025-10-13 10:01:12 +0200

    Remove all pages except Dependency Graph
    - Updated plugin.json to only include Dependency Graph and Configuration
    - Removed routes for PageOne, PageTwo, PageThree, PageFour from constants.ts
    - Simplified App.tsx to only route to DependencyGraph
    - Deleted unused page components (PageOne, PageTwo, PageThree, PageFour)
    - Set Dependency Graph as the default page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,19 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { AppRootProps } from '@grafana/data';
 import { ROUTES } from '../../constants';
-import React from 'react';
 
-const PageOne = React.lazy(() => import('../../pages/PageOne'));
-const PageTwo = React.lazy(() => import('../../pages/PageTwo'));
-const PageThree = React.lazy(() => import('../../pages/PageThree'));
-const PageFour = React.lazy(() => import('../../pages/PageFour'));
 const DependencyGraph = React.lazy(() => import('../../pages/DependencyGraph'));
 
 function App(props: AppRootProps) {
   return (
     <Routes>
-      <Route path={ROUTES.Two} element={<PageTwo />} />
-      <Route path={`${ROUTES.Three}/:id?`} element={<PageThree />} />
-
-      {/* Full-width page (this page will have no side navigation) */}
-      <Route path={ROUTES.Four} element={<PageFour />} />
-
       {/* Dependency Graph page */}
       <Route path={ROUTES.DependencyGraph} element={<DependencyGraph />} />
 
       {/* Default page */}
-      <Route path="*" element={<PageOne />} />
+      <Route path="*" element={<DependencyGraph />} />
     </Routes>
   );
 }
