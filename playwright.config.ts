b9a2e11bd221d229c4fe632a33c21f03ce970cb3
Erik Sundell <erik.sundell87@gmail.com>
2025-11-24 14:02:15 +0100

    temp no retires

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,8 @@ export default defineConfig<PluginOptions>({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Timeout settings - increased for CI environments */
@@ -31,7 +32,7 @@ export default defineConfig<PluginOptions>({
     baseURL: process.env.GRAFANA_URL || 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
