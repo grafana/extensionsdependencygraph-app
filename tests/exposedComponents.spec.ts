488d83a8d74feb19aa9cc9d8ba4403d828e78891
Erik Sundell <erik.sundell87@gmail.com>
2025-11-25 08:33:54 +0100

    skip some tests

--- tests/exposedComponents.spec.ts
+++ tests/exposedComponents.spec.ts
@@ -1,3 +1,4 @@
+import * as semver from 'semver';
 import {
   EXPECTED_COUNTS,
   assertUrlParam,
@@ -13,7 +14,8 @@ test.describe('Exposed Components View', () => {
     await depGraphPageWithMockApps.goto({ path: 'dependency-graph?view=exposedComponents' });
   });
 
-  test('shows correct selectors', async ({ depGraphPageWithMockApps }) => {
+  test('shows correct selectors', async ({ depGraphPageWithMockApps, grafanaVersion }) => {
+    test.skip(semver.lt(grafanaVersion, '12.2.0'), 'Combobox not reliably testable before Grafana 12.2');
     const { page } = depGraphPageWithMockApps.ctx;
 
     await expect(page.getByTestId(dependencyGraphTestIds.visualizationModeSelector)).toBeVisible();
