06220b76fecf32f8a49a9f4a933c0524ef758f6d
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2026-07-31 10:57:39 +0200

    fix: drop react-router-dom, use locationService for URL state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-
 import { AppRootProps } from '@grafana/data';
-import { ROUTES } from '../constants';
 import React from 'react';
 
 const DependencyGraph = React.lazy(() => import('../pages/DependencyGraph'));
 
 function App(props: AppRootProps) {
-  return (
-    <Routes>
-      {/* Dependency Graph page */}
-      <Route path={ROUTES.DependencyGraph} element={<DependencyGraph />} />
-
-      {/* Default page */}
-      <Route path="*" element={<DependencyGraph />} />
-    </Routes>
-  );
+  return <DependencyGraph />;
 }
 
 export default App;
